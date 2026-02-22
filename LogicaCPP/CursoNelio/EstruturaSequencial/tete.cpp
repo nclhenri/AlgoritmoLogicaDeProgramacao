2205d9dc2b58448de307aa7b729ca3b2e9e25006
nclhenri <nicolashenrisilva@outlook.com>
2026-02-22 19:30:28 -0300

    tetado

---
+++ LogicaCPP/CursoNelio/EstruturaSequencial/tete.cpp
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main(){
+
+
+
+    printf("tetando");
+
+
+
+    return 0;
+}}
